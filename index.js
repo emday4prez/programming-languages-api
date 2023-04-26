dc650c2a70aa98eb189854b68047d5cb23a9a124
Emerson <emerson.day@gmail.com>
2023-04-25 17:11:04 -0700

    created file structure

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'ok' });
+});
+
+app.listen(port, () => console.log('listening on port 3000'));
